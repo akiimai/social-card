3525e4124e8167c5d5d9cf80c1634618a613a8f7
Aki Imai <akimai808@gmail.com>
2019-04-29 13:13:35 -0700

    add card

--- client/src/SocialCard.js
+++ client/src/SocialCard.js
@@ -1,16 +1,33 @@
-import React from 'react'; 
-import './SocialCard.css'; 
-import Front from './Front'; 
-import Back from './Back'; 
+import React from 'react';
+import './SocialCard.css';
+import Front from './components/Front';
+import Back from './components/Back';
+import BioFront from './components/BioFront';
 
 class Card extends React.Component {
     render() {
         return (
-            <div className='card-container'>
-                <div className='card-body'>
+            <div className='card-box'>
+                <div className="card" id="bio">
+                    <BioFront />
+                </div>
+                <div className="card" id="skills">
+                </div>
+                <div className="card" id="interests">
+                </div>
+
+                {/* <div className='card-body'>
+                    <Front />
+                    <Back />
+                </div> */}
+                {/* <div className='card-body'>
                     <Front />
                     <Back />
                 </div>
+                <div className='card-body'>
+                    <Front />
+                    <Back />
+                </div> */}
             </div>
         )
     }
